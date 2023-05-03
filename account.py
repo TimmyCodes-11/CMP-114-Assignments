4ea712481d7cfa2f1590c42750772172dbfe89c8
TimmyCodes-11 <solomon.akinyogbon-timilehim@Binghamuni.edu.ng>
2023-05-03 09:36:24 +0100

    updated account details

--- account.py
+++ account.py
@@ -1,1 +1,15 @@
 class Account:
+    name = "John Jude"
+    account_number = "0637798724"
+    account_balance = "1000"
+
+def __init__(self):
+    name = "John Jude"
+    account_number = "0637798724"
+    account_balance = "1000"
+
+def deposit (self, amount=None):
+    self.balance = self.balance + amount
+    print
+def withdrawal (self, amount=None):
+    self.balance = self.balance - amount
